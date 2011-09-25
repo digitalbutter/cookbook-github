bea3bcc27d187503f74304f4325451de855c3bc7
edbo <ed@butter.com.hk>
2011-09-25 14:52:31 +0800

    removing more annoying git tags

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,14 +32,6 @@ end
   r.push gem_package pack do
     action :nothing
   end
-=======
-r.push gem_package "git-ssh-wrapper" do
-  action :nothing
-end
-
-r.push gem_package "octopi" do
-  action :nothing
->>>>>>> 60d0b59a676ed9cf565d49e943a734ce3ce80177
 end
 
 r.each do |pack|
